7794db508bee3f9c19fbf5beccee0ab53b076025
strawmanbobi <strawmanbobi@163.com>
2026-07-12 18:44:05 +0800

    fixed win32 example

--- win32-example/ir_decoder/src/include/ir_defs.h
+++ win32-example/ir_decoder/src/include/ir_defs.h
@@ -12,7 +12,7 @@ Revision log:
 #ifndef _IR_DEFS_H
 #define _IR_DEFS_H
 
-#define IR_DECODE_LIB_VER "1.5.2"
+#define IR_DECODE_LIB_VER "1.5.3"
 
 #if defined (BOARD_PC)
 #pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
